e7d6b5584c8ea2804073de3461adf43c1f0b7f3a
Ivan <akkuratnov.98@yandex.ru>
2021-12-19 23:56:11 +0300

    commit

--- src/test/java/Main/TestSuite/TestLogInPage.java
+++ src/test/java/Main/TestSuite/TestLogInPage.java
@@ -31,17 +31,13 @@ public class TestLogInPage {
     @BeforeTest
     public void openLogIn() {
         try {
-
             System.setProperty("webdriver.chrome.driver", String.valueOf(Paths.get("src/main/resources/tools/chromedriver.exe")));
             driver = new ChromeDriver();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.get("https://lmslite47vr.demo.mirapolis.ru/mira");
-
             mainPage = PageFactory.initElements(driver, MainPage.class);
             logInPage = PageFactory.initElements(driver, LogInPage.class);
             passwordRecoveryPage = PageFactory.initElements(driver, PasswordRecoveryPage.class);
-
-
         } catch (Exception e) {
             System.out.println("Не удалось открыть страницу сайта");
         }
@@ -49,64 +45,47 @@ public class TestLogInPage {
 
     @Test(testName = "Переход на страницу востановления пароля", priority = 2)
     public void SwitchToRecoveryPage(){
-
         logInPage.switchToRecoveryPage();
-
-
         Assert.assertEquals(passwordRecoveryPage.getInfoTitleText(), "Восстановление пароля");
-
         logInPage.goToLogInPage();
     }
 
     @Parameters({"invalidLogin", "password"})
     @Test(testName = "Неверный Ввод Логина пользователя для авторизации", priority = 1)
     public void invalidLoginTest(String login, String password){
-
         logInPage.inputLogin(login);
         logInPage.inputPassword(password);
         logInPage.enterLoginPage();
-
         Assert.assertEquals("Неверные данные для авторизации", logInPage.getTextAlert());
-
     }
 
     @Parameters({"password"})
     @Test(testName = "Ввод пустого Логина пользователя для авторизации", priority = 1)
     public void emptyLoginTest(String password){
-
         String emptyLogin = "";
-
         logInPage.inputLogin(emptyLogin);
         logInPage.inputPassword(password);
         logInPage.enterLoginPage();
-
         Assert.assertEquals("Неверные данные для авторизации.", logInPage.getTextAlert());
-
     }
 
     @Parameters({"login"})
     @Test(testName = "Ввод пустого пароля пользователя для авторизации", priority = 1)
     public void emptyPasswordTest(String login){
-
         String emptyPassword = "";
-
         logInPage.inputLogin(login);
         logInPage.inputPassword(emptyPassword);
         logInPage.enterLoginPage();
-
         Assert.assertEquals("Неверные данные для авторизации.", logInPage.getTextAlert()); //При пустых полях появляется точка
-
     }
 
 
     @Parameters({"login", "invalidPassword"})
     @Test(testName = "Неверный Ввод Логина пользователя для авторизации", priority = 1)
     public void invalidPasswordTest(String login, String password){
-
         logInPage.inputLogin(login);
         logInPage.inputPassword(password);
         logInPage.enterLoginPage();
-
         Assert.assertEquals("Неверные данные для авторизации", logInPage.getTextAlert()); //При пустых полях появляется точка
 
     }
@@ -114,19 +93,16 @@ public class TestLogInPage {
     @Parameters({"login", "password"})
     @Test(testName = "Ввод Верного Логина пользователя для авторизации", priority = 3)
     public void correctLoginTest(String login, String password){
-
         logInPage.inputLogin(login);
         logInPage.inputPassword(password);
         logInPage.enterLoginPage();
-
         Assert.assertEquals("Фомина Елена Сергеевна", mainPage.getFullNameText("title"));
 
     }
 
     @Test(testName = "Кнопка показа пароля", priority = 1)
     public void showPassword(){
-
-        Assert.assertEquals("password", logInPage.passwordFieldElementAttribute("type"));        ;
+        Assert.assertEquals("password", logInPage.passwordFieldElementAttribute("type"));
         logInPage.clickShowPassword();
         Assert.assertEquals("text", logInPage.passwordFieldElementAttribute("type"));
         logInPage.clickShowPassword();
@@ -136,11 +112,9 @@ public class TestLogInPage {
 
     @Test(testName = "Проверка максимальной длины input login", priority = 1)
     public void testMaxLengthLogin(){
-
         StringBuilder lengthLogin = new StringBuilder();
         lengthLogin.append("0123456789".repeat(16));
         logInPage.inputLogin(lengthLogin.toString());
-
         Assert.assertEquals(150, logInPage.loginFieldAttribute("value").length());
     }
 
--- src/test/java/Main/TestSuite/TestRecoveryPage.java
+++ src/test/java/Main/TestSuite/TestRecoveryPage.java
@@ -24,15 +24,12 @@ public class TestRecoveryPage {
     @BeforeTest
     public void SwitchToRecoveryPage() {
         try {
-
             System.setProperty("webdriver.chrome.driver", String.valueOf(Paths.get("src/main/resources/tools/chromedriver.exe")));
             driver = new ChromeDriver();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
             driver.get("https://lmslite47vr.demo.mirapolis.ru/mira");
-
             logInPage = PageFactory.initElements(driver, LogInPage.class);
             passwordRecoveryPage = PageFactory.initElements(driver, PasswordRecoveryPage.class);
-
             logInPage.switchToRecoveryPage();
         } catch (Exception e) {
             System.out.println("Не удалось открыть страницу сайта");
@@ -42,49 +39,38 @@ public class TestRecoveryPage {
     @Parameters({"invalidEmail"})
     @Test(testName = "Ввод неправильного Email для востановления пароля", priority = 1)
     public void inputEmail(String invalidEmail) {
-
         passwordRecoveryPage.inputLogin(invalidEmail);
         passwordRecoveryPage.clickEnter();
-
         Assert.assertEquals(passwordRecoveryPage.waitDivFailed(), "Пользователь с таким именем не найден.");
     }
 
     @Parameters({"invalidLogin"})
     @Test(testName = "Ввод неправильного логина для востановления пароля", priority = 1)
     public void inputInvalidLogin(String invalidLogin) {
-
         passwordRecoveryPage.inputLogin(invalidLogin);
         passwordRecoveryPage.clickEnter();
-
         Assert.assertEquals(passwordRecoveryPage.waitDivFailed(), "Пользователь с таким именем не найден.");
     }
 
     @Test(testName = "Возврат на стриницу Авторизации", priority = 2)
     public void switchToLogInPage() {
-
         passwordRecoveryPage.backLoginPage();
-
         Assert.assertEquals(logInPage.getInfoTitleText(), "Вход в систему");
-
         logInPage.switchToRecoveryPage();
     }
 
     @Test(testName = "Переход на страницу авторизации через Logo компании", priority = 2)
     public void goToLogInPage() {
         passwordRecoveryPage.reloadLogInPageWithLogo();
-
         Assert.assertEquals(logInPage.getInfoTitleText(), "Вход в систему");
-
         logInPage.switchToRecoveryPage();
     }
 
     @Parameters({"login"})
     @Test(testName = "Ввод правильного логина для востановления пароля", priority = 3)
     public void inputCorrectLogin(String login) {
-
         passwordRecoveryPage.inputLogin(login);
         passwordRecoveryPage.clickEnter();
-
         Assert.assertEquals(passwordRecoveryPage.waitDivSuccess(), "На ваш электронный адрес отправлена инструкция по восстановлению пароля.");
     }
 
