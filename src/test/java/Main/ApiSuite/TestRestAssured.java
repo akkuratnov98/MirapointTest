debcd228b6230cd9523a01c49e796398141fee96
Ivan <akkuratnov.98@yandex.ru>
2022-01-07 16:00:03 +0300

    Rest Assured + JDBC

--- src/test/java/Main/ApiSuite/TestRestAssured.java
+++ src/test/java/Main/ApiSuite/TestRestAssured.java
@@ -1,9 +1,15 @@
 package Main.ApiSuite;
 
 import static io.restassured.RestAssured.*;
+import static io.restassured.config.JsonConfig.jsonConfig;
 import static io.restassured.matcher.RestAssuredMatchers.*;
+import static io.restassured.path.json.config.JsonPathConfig.NumberReturnType.BIG_DECIMAL;
 import static  org.hamcrest.Matchers.*;
+
+import io.restassured.RestAssured;
 import org.testng.annotations.Test;
+
+import java.math.BigDecimal;
 
 //import io.restassured.module.jsv.JsonSchemaValidator.*;
 
@@ -13,13 +19,14 @@ public class TestRestAssured {
     @Test
     public void firstTest() {
         get("/lotto").then().body("lotto.lottoId", equalTo(5));
+        get("/lotto").then().body("lotto.winners.winnerId", hasItems(23, 54));
 
         given().
                 config(RestAssured.config().jsonConfig(jsonConfig().numberReturnType(BIG_DECIMAL))).
                 when().
                 get("/price").
                 then().
-                body("price", is(new BigDecimal(12.12));
+                body("price", is(new BigDecimal(12.12)));
 
     }
 }
