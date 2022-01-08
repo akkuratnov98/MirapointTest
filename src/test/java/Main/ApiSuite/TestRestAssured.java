4ab9183de9ca75bc869bdd7cd0cdd1bf04291e38
Ivan <akkuratnov.98@yandex.ru>
2022-01-08 19:20:00 +0300

    rest assure and jeckson and lombook

--- src/test/java/Main/ApiSuite/TestRestAssured.java
+++ src/test/java/Main/ApiSuite/TestRestAssured.java
@@ -4,8 +4,12 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.config.JsonConfig.jsonConfig;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static io.restassured.path.json.config.JsonPathConfig.NumberReturnType.BIG_DECIMAL;
+//import static jdk.internal.logger.DefaultLoggerFinder.SharedLoggers.application;
 import static  org.hamcrest.Matchers.*;
 
+import Main.Pojo.PojoClass;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.restassured.RestAssured;
 import org.testng.annotations.Test;
 
@@ -28,5 +32,36 @@ public class TestRestAssured {
                 then().
                 body("price", is(new BigDecimal(12.12)));
 
+    }
+
+    @Test
+    public void secondTest() {
+        given().
+                when().
+                get("https://reqres.in/api/users/2")
+                .then()
+                .statusCode(200)
+                .header("content-type", "application/json; charset=utf-8")
+                .log().all();
+    }
+
+    @Test
+    public void testPojo() throws JsonProcessingException {
+        PojoClass pojoClass = new PojoClass("Ivan");
+        pojoClass.setJob("tester");
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonString = mapper.writeValueAsString(pojoClass);
+        String s =
+        given().
+                headers("Content-Type", "application/json")
+                .body(jsonString)
+        .when()
+                .post("https://reqres.in/api/users")
+        .then()
+                .statusCode(201)
+                .log().all()
+                .extract()
+                .body().asString();
+        System.out.println(s);
     }
 }
---
+++ src/test/java/Main/Pojo/PojoClass.java
@@ -0,0 +1,11 @@
+package Main.Pojo;
+
+import lombok.Data;
+
+@Data
+public class PojoClass {
+    private final String name;
+    private String job;
+
+
+}
